8f52db53dfa79595fb90631beac18c4cc0e60000
orzechdev <tarasiukdawid@gmail.com>
2019-04-11 22:34:43 +0200

    fix

--- csp_warmup.py
+++ csp_warmup.py
@@ -2,20 +2,25 @@ from utils import print_title
 import numpy as np
 
 
+possible_values_size = 5
+
+
 def start():
     print_title('CSP WARM-UP: start')
 
     print_title('CSP WARM-UP: init square with size 5')
-    square = init_square(4)
-    print(square)
+    # square = init_square(5)
+    # print(square)
+    #
+    # print_title('CSP WARM-UP: set possible values')
+    # possible_values = {1, 2, 3, 4, 5}
+    # print(possible_values)
+    #
+    # print_title('CSP WARM-UP: fill square non repeatedly')
+    # square_filled = fill_square_non_repeatedly(square, possible_values)
+    # print(square_filled)
 
-    print_title('CSP WARM-UP: set possible values')
-    possible_values = {1, 2, 3, 4, 5}
-    print(possible_values)
-
-    print_title('CSP WARM-UP: fill square non repeatedly')
-    square_filled = fill_square_non_repeatedly(square, possible_values)
-    print(square_filled)
+    print_latin(7)
 
     print_title('CSP WARM-UP: end')
 
@@ -40,4 +45,30 @@ def fill_square_non_repeatedly(square, possible_vals):
                 square[row, col] = possible_vals_in_col.pop()
 
     return square
+
+
+# Function to prn x n Latin Square
+def print_latin(n):
+    # A variable to control the
+    # rotation point.
+    k = n + 1
+
+    # Loop to prrows
+    for i in range(1, n + 1, 1):
+
+        # This loops runs only after first
+        # iteration of outer loop. It prints
+        # numbers from n to k
+        temp = k
+        while temp <= n:
+            print(temp, end=" ")
+            temp += 1
+
+        # This loop prints numbers
+        # from 1 to k-1.
+        for j in range(1, k):
+            print(j, end=" ")
+
+        k -= 1
+        print()
 
